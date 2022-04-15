8e372cb571f44f4ee44e3f0ff7923693d0161337
gabriel.compasso <GABRIEL.COMPASSO@fleetcor.com.br>
2022-04-15 14:32:16 -0300

    chore: add aws-resources and env to config and add dynanamodblocal migrations

---
+++ src/infra/migrations/create-payment-manager.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-console */
+const { DYNAMODB } = require('../../main/config/aws-resources');
+const { PAYMENT_TABLE_NAME, NODE_ENV } = require('../../main/config/env');
+
+const params = {
+  TableName: PAYMENT_TABLE_NAME,
+  AttributeDefinitions: [
+    {
+      AttributeName: 'PK',
+      AttributeType: 'S'
+    },
+    {
+      AttributeName: 'SK',
+      AttributeType: 'S'
+    },
+    {
+      AttributeName: 'email',
+      AttributeType: 'S'
+    }
+  ],
+  KeySchema: [
+    {
+      AttributeName: 'PK',
+      KeyType: 'HASH'
+    },
+    {
+      AttributeName: 'SK',
+      KeyType: 'RANGE'
+    }
+  ],
+  GlobalSecondaryIndexes: [
+    {
+      IndexName: 'email-index',
+      KeySchema: [
+        {
+          AttributeName: 'email',
+          KeyType: 'HASH'
+        }
+      ],
+      Projection: {
+        ProjectionType: 'ALL'
+      },
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1
+      }
+    }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5
+  }
+};
+
+const createTable = async () => {
+  const response = await DYNAMODB.createTable(params).promise();
+  console.log(JSON.stringify(response));
+};
+
+const deleteTable = async () => {
+  const response = await DYNAMODB.deleteTable({ TableName: PAYMENT_TABLE_NAME }).promise();
+  console.log(JSON.stringify(response));
+};
+
+const handler = () => {
+  const operation = process.argv[2];
+  if (operation === 'start') return createTable();
+
+  const isDevOrTest = NODE_ENV === 'dev' || NODE_ENV === 'test';
+  if (operation === 'undo') return isDevOrTest ? deleteTable() : null;
+
+  return console.log(`Invalid parameter: ${process.argv[2]}`);
+};
+
+handler();
---
+++ src/main/config/env.js
@@ -0,0 +1,8 @@
+const REGION = process.env.REGION || 'localhost';
+
+module.exports = {
+  REGION,
+  NODE_ENV: process.env.NODE_ENV || 'dev',
+  DYNAMODB_ENDPOINT: (REGION === 'localhost' && 'http://localhost:8000') || null,
+  PAYMENT_TABLE_NAME: process.env.PAYMENT_MANAGER_TABLE_NAME || 'payment-manager'
+};
