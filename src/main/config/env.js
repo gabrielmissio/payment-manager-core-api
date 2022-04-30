00c003351dded80969b63667e1879084f1a8bd9e
gabriel.compasso <GABRIEL.COMPASSO@fleetcor.com.br>
2022-04-30 18:03:54 -0300

    chore: add full access rule to dynamodb

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -4,5 +4,5 @@ module.exports = {
   REGION,
   NODE_ENV: process.env.NODE_ENV || 'dev',
   DYNAMODB_ENDPOINT: (REGION === 'localhost' && 'http://localhost:8000') || null,
-  PAYMENT_MANAGER_TABLE_NAME: process.env.PAYMENT_MANAGER_TABLE_NAME || 'payment-manager'
+  PAYMENT_MANAGER_TABLE_NAME: (REGION === 'localhost' && 'payment-manager') || process.env.PAYMENT_MANAGER_TABLE_NAME
 };
